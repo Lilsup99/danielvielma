afc78e969a230f3219b52f5b964ec994250aa277
Daniel Vielma <124645048+Lilsup99@users.noreply.github.com>
2024-10-13 18:04:49 +0000

    new_component

---
+++ DanielVielma/src/presentacion.jsx
@@ -0,0 +1,20 @@
+import './intro.css'
+
+
+function Intro() {
+
+  return (
+    <>
+      <div className="container">
+        <img src="https://cdn-icons-png.flaticon.com/512/1053/1053244.png" alt="image" />
+        <span>
+            <h1>Daniel Vielma</h1>
+            <h3>Data scientist</h3>
+        </span>
+      </div>
+
+    </>
+  )
+}
+
+export default Intro
